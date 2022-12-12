e58407d15df168411ad0d3430e1110e65e11686a
shafinach04 <shafinachaerunisa@gmail.com>
2022-12-12 11:50:51 +0700

    add ratings

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,7 +4,11 @@ const Reviews = () => {
   return (
       <section id="Reviews" className="flex h-screen pl-10 font-['Helvetica']">
         <div className="my-auto justify-end">
-        <p className="text-5xl font-bold">Reviews Section</p>
+        <p className="text-5xl font-bold">Reviews Section<br/></p>
+        <div><br/>&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+        <p className="text-1xl font-bold">LOVE MY NEW THETUMBLERCO</p>
+        <p className="text-lg" align="justify-center">I love this cup, the color is beautiful. I have used these tumblers for 5+ years <br/>and never had issues they last forever, fit in my cup holder are larger size<br/> than most others on the market and they stay hot/cold for many hours!</p>
+        <p className="text-lg">─ BRITTNEY W.</p>
         </div>
       </section>
   );
